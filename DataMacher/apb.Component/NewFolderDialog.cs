41d7ec007b873cebf05796a4eb11767928d8137b
Andi <r4pp3r_ul@yahoo.com>
2019-01-20 18:25:26 +0200

    Terminat dialog, nou design

--- DataMacher/apb.Component/NewFolderDialog.cs
+++ DataMacher/apb.Component/NewFolderDialog.cs
@@ -28,9 +28,8 @@ namespace DataMacher
 
         public void InitDialog()
         {
-            prompt.RightToLeft = RightToLeft.Yes;
-            prompt.Width = 800;
-            prompt.Height = 800;
+            prompt.Width = 560;
+            prompt.Height = 380;
             TableLayoutPanel tableLayoutPanel = new TableLayoutPanel();
             tableLayoutPanel.RowCount = 4;
             tableLayoutPanel.ColumnCount = 1;
@@ -45,7 +44,7 @@ namespace DataMacher
 
 
             //Panel Dosar
-            Panel panelDetaliiDosar = CreateDosar(100);
+            Panel panelDetaliiDosar = CreateDosar(70);
             // Label si input
             RenderNumeClient(panelDetaliiDosar);
             // Label si input
@@ -61,30 +60,48 @@ namespace DataMacher
 
 
 
-            //Panel Dosar
-            Panel panelTipAct = CreateDosar(100);
+            //Panel Redactata
+            Panel panelStareRedactata = CreateDosar(70);
             // Label si input
-            RenderNumeStare(panelTipAct);
+            RenderNumeRedactata(panelStareRedactata);
             // Label si input
-            RenderDataStare(panelTipAct);
+            RenderDataRedactata(panelStareRedactata);
             // Label si input
-            RenderAdresaStare(panelTipAct);
+            RenderNumeComunicata(panelStareRedactata);
             // Label si input
-            RenderNumeAvocat(panelTipAct);
+            RenderDataComunicata(panelStareRedactata);
             // Label si input
-            RenderInstanta(panelTipAct);
-            // Label si input
-            RenderJudecatorie(panelTipAct);
+            RenderAdresaComunicata(panelStareRedactata);
 
-            //Panel Dosar
-            Panel panelStareAct = CreateDosar(40);
+            Panel panelStareApelRecurs = CreateDosar(120);
             // Label si input
-            RenderFotoCopiat(panelStareAct);
+            RenderNumeApelRecurs(panelStareApelRecurs);
+            // Label si input
+            RenderDataApelRecurs(panelStareApelRecurs);
+            // Label si input
+            RenderNumeAvocat(panelStareApelRecurs);
+            // Label si input
+            RenderNumePlecat(panelStareApelRecurs);
+            // Label si input
+            RenderDataPlecat(panelStareApelRecurs);
+            // Label si input
+            RenderInstanta(panelStareApelRecurs);
+            // Label si input
+            RenderNumeInregistrat(panelStareApelRecurs);
+            // Label si input
+            RenderDataInregistrat(panelStareApelRecurs);
+            // Label si input
+            RenderJudecatorie(panelStareApelRecurs);
+            // Label si input
+            RenderFotoCopiat(panelStareApelRecurs);
+            // Label si input
+            RenderMentiuni(panelStareApelRecurs);
+
 
             tableLayoutPanel.Controls.Add(panelCodDosar, 0, 0);
             tableLayoutPanel.Controls.Add(panelDetaliiDosar, 1, 0);
-            tableLayoutPanel.Controls.Add(panelTipAct, 2, 0);
-            tableLayoutPanel.Controls.Add(panelStareAct, 3, 0);
+            tableLayoutPanel.Controls.Add(panelStareRedactata, 2, 0);
+            tableLayoutPanel.Controls.Add(panelStareApelRecurs, 3, 0);
             tableLayoutPanel.Dock = DockStyle.Fill;
         }
 
@@ -115,13 +132,16 @@ namespace DataMacher
             textLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             return textLabel;
         }
-
+        /// <summary>
+        /// ////////
+        /// </summary>
+        /// <param name="panel"></param>
         private void RenderIndicativInstanta(Panel panel)
         {
-            Label textLabeLIndicativInstanta = CreateLabel("Indicativ Instanta" ,170,15,120,20);
+            Label textLabeLIndicativInstanta = CreateLabel("Indicativ Instanta" ,60,15,120,20);
             panel.Controls.Add(textLabeLIndicativInstanta);
 
-            ComboBox comboBoxIndicativInstanta = new ComboBox() { Left = 180, Top = 35, Name = "textBoxIndicativInstanta" };
+            ComboBox comboBoxIndicativInstanta = new ComboBox() { Left = 70, Top = 35, Name = "textBoxIndicativInstanta" };
             comboBoxIndicativInstanta.Size = new System.Drawing.Size(100, 20);
             comboBoxIndicativInstanta.DropDownStyle = ComboBoxStyle.DropDownList;
             comboBoxIndicativInstanta.SelectedIndexChanged += new EventHandler(ComboBoxIndicativInstanta_SelectedIndexChanged);
@@ -132,116 +152,174 @@ namespace DataMacher
 
         private void RenderNrDosar(Panel panel)
         {
-            Label textLabeLNrDosar = CreateLabel("Numar Dosar", 435, 15, 120, 20);
+            Label textLabeLNrDosar = CreateLabel("Numar Dosar", 300, 15, 120, 20);
             panel.Controls.Add(textLabeLNrDosar);
 
-            TextBox textBoxCodDosar1 = CreateTextBox("textBoxCodDosar1", "", 370, 35, 70, 20);
-            TextBox textBoxCodDosar2 = CreateTextBox("textBoxCodDosar2", "", 450, 35, 50, 20);
-            TextBox textBoxCodDosar3 = CreateTextBox("textBoxCodDosar3", "", 510, 35, 40, 20);
-            TextBox textBoxCodDosar4 = CreateTextBox("textBoxCodDosar4", "", 560, 35, 40, 20);
+            TextBox textBoxCodDosar1 = CreateTextBox("textBoxCodDosar1", "", 235, 35, 70, 20);
+            TextBox textBoxCodDosar2 = CreateTextBox("textBoxCodDosar2", "", 315, 35, 50, 20);
+            TextBox textBoxCodDosar3 = CreateTextBox("textBoxCodDosar3", "", 375, 35, 40, 20);
+            TextBox textBoxCodDosar4 = CreateTextBox("textBoxCodDosar4", "", 425, 35, 40, 20);
             panel.Controls.Add(textBoxCodDosar1);
             panel.Controls.Add(textBoxCodDosar2);
             panel.Controls.Add(textBoxCodDosar3);
             panel.Controls.Add(textBoxCodDosar4);
         }
-
+        /// <summary>
+        /// /////////////////////
+        /// </summary>
+        /// <param name="panel"></param>
         private void RenderNumeClient(Panel panel)
         {
-            TextBox textBoxNumeClient = CreateTextBox("textBoxNumeClient", "Nume Client", 100, 20, 200, 20);
+            TextBox textBoxNumeClient = CreateTextBox("textBoxNumeClient", "Nume Client", 20, 20, 200, 20);
             panel.Controls.Add(textBoxNumeClient);
         }
 
         private void RenderNumeTitular(Panel panel)
         {           
-            TextBox textBoxNumeTitular = CreateTextBox("textBoxNumeTitular", "Nume titular", 400, 20, 300, 20);
+            TextBox textBoxNumeTitular = CreateTextBox("textBoxNumeTitular", "Nume titular", 240, 20, 280, 20);
             panel.Controls.Add(textBoxNumeTitular);
         }
 
         private void RenderTipAct(Panel panel)
         {
-            TextBox textBoxTipAct = CreateTextBox("textBoxtTipAct", "Tip act", 100, 50, 100, 20);
+            TextBox textBoxTipAct = CreateTextBox("textBoxtTipAct", "Tip act", 20, 50, 100, 20);
             panel.Controls.Add(textBoxTipAct);
         }
 
         private void RenderDataAct(Panel panel)
         {
-            TextBox textBoxDataAct = CreateTextBox("textBoxDataAct", "Data Act", 200, 50, 100, 20);
+            TextBox textBoxDataAct = CreateTextBox("textBoxDataAct", "Data Act", 150, 50, 100, 20);
             panel.Controls.Add(textBoxDataAct);
         }
 
         private void RenderDetaliiAct(Panel panel)
         {
-            TextBox textBoxDetaliiAct = CreateTextBox("textBoxDetaliiAct", "Detalii Act", 450, 50, 100, 20);
+            TextBox textBoxDetaliiAct = CreateTextBox("textBoxDetaliiAct", "Detalii Act", 290, 50, 100, 20);
             panel.Controls.Add(textBoxDetaliiAct);
         }
 
         private void RenderZileRamase(Panel panel)
         {
-            TextBox textBoxZileRamase = CreateTextBox("textBoxZileRamase", "Zile Ramase", 550, 50, 100, 20);
+            TextBox textBoxZileRamase = CreateTextBox("textBoxZileRamase", "Zile Ramase", 420, 50, 100, 20);
             panel.Controls.Add(textBoxZileRamase);
         }
-
-        private void RenderNumeStare(Panel panel)
+        /// <summary>
+        /// /////////////
+        /// </summary>
+        /// <param name="panel"></param>
+        /// 
+        private void RenderNumeRedactata(Panel panel)
         {
-            Label textLabelNumeStare = new Label() { Left = 50, Top = 20, Text = "Stare" };
-            panel.Controls.Add(textLabelNumeStare);
-            TextBox textBoxNumeStare = new TextBox() { Left = 150, Top = 20, Name = "textBoxNumeStare" };
-            textBoxNumeStare.Size = new System.Drawing.Size(100, 20);
-            panel.Controls.Add(textBoxNumeStare);
+            TextBox textBoxNumeRedactata = CreateTextBox("textBoxNumeRedactata", "Redactata?", 20, 20, 100, 20);
+            panel.Controls.Add(textBoxNumeRedactata);
         }
 
-        private void RenderDataStare(Panel panel)
+        private void RenderDataRedactata(Panel panel)
         {
-            Label textLabelDataStare = new Label() { Left = 50, Top = 50, Text = "Data Stare" };
-            panel.Controls.Add(textLabelDataStare);
-            TextBox textBoxDataStare = new TextBox() { Left = 150, Top = 50, Name = "textBoxDataStare" };
-            textBoxDataStare.Size = new System.Drawing.Size(100, 20);
-            panel.Controls.Add(textBoxDataStare);
+            TextBox textBoxDataRedactata = CreateTextBox("textBoxDataRedactata", "Data Redactare", 150, 20, 100, 20);
+            panel.Controls.Add(textBoxDataRedactata);
         }
 
-        private void RenderAdresaStare(Panel panel)
+        private void RenderNumeComunicata(Panel panel)
         {
-            Label textLabelAdresaStare = new Label() { Left = 50, Top = 80, Text = "Adresa" };
-            panel.Controls.Add(textLabelAdresaStare);
-            TextBox textBoxAdresaStare = new TextBox() { Left = 150, Top = 80, Name = "textBoxAdresaStare" };
-            textBoxAdresaStare.Size = new System.Drawing.Size(100, 20);
-            panel.Controls.Add(textBoxAdresaStare);
+            TextBox textBoxNumeComunicata = CreateTextBox("textBoxNumeComunicata", "Comunicata?", 290, 20, 100, 20);
+            panel.Controls.Add(textBoxNumeComunicata);
+        }
+
+        private void RenderDataComunicata(Panel panel)
+        {
+            TextBox textBoxDataComunicata = CreateTextBox("textBoxDataComunicata", "Data Comunicata", 420, 20, 100, 20);
+            panel.Controls.Add(textBoxDataComunicata);
+        }
+
+        private void RenderAdresaComunicata(Panel panel)
+        {
+            TextBox textBoxDataComunicata = CreateTextBox("textBoxAdresaComunicata", "Adresa Comunicata", 20, 50, 500, 20);
+            panel.Controls.Add(textBoxDataComunicata);
+        }
+        /// <summary>
+        /// /////////////
+        /// </summary>
+        /// <param name="panel"></param>
+        /// 
+
+
+        /// <summary>
+        /// /////////////
+        /// </summary>
+        /// <param name="panel"></param>
+        /// 
+
+        private void RenderNumeApelRecurs(Panel panel)
+        {
+            TextBox textBoxNumeApelRecurs = CreateTextBox("textBoxNumeApelRecurs", "Apel/Recurs", 20, 20, 150, 20);
+            panel.Controls.Add(textBoxNumeApelRecurs);
+        }
+
+        private void RenderDataApelRecurs(Panel panel)
+        {
+            TextBox textBoxDataApelRecurs = CreateTextBox("textBoxDataApelRecurs", "Data Apel/Recurs", 190, 20, 150, 20);
+            panel.Controls.Add(textBoxDataApelRecurs);
         }
 
         private void RenderNumeAvocat(Panel panel)
         {
-            Label textLabelNumeAvct = new Label() { Left = 350, Top = 20, Text = "Nume Avocat" };
-            panel.Controls.Add(textLabelNumeAvct);
-            TextBox textBoxNumeAvc = new TextBox() { Left = 450, Top = 20, Name = "textBoxNumeAvc" };
-            textBoxNumeAvc.Size = new System.Drawing.Size(100, 20);
+            TextBox textBoxNumeAvc = CreateTextBox("textBoxNumeAvc", "Nume Avocat", 370, 20, 150, 20);
             panel.Controls.Add(textBoxNumeAvc);
+        }
+
+        /// <summary>
+        /// /////////////
+        /// </summary>
+        /// <param name="panel"></param>
+        /// 
+
+        private void RenderNumePlecat(Panel panel)
+        {
+            TextBox textBoxNumePlecat = CreateTextBox("textBoxNumePlecat", "A plecat dosarul?", 20, 20, 150, 20);
+            panel.Controls.Add(textBoxNumePlecat);
+        }
+
+        private void RenderDataPlecat(Panel panel)
+        {
+            TextBox textBoxDataPlecat = CreateTextBox("textBoxDataPlecat", "Data plecare dosar", 190, 20, 150, 20);
+            panel.Controls.Add(textBoxDataPlecat);
         }
 
         private void RenderInstanta(Panel panel)
         {
-            Label textLabelInstanta = new Label() { Left = 350, Top = 50, Text = "Instanta" };
-            panel.Controls.Add(textLabelInstanta);
-            TextBox textBoxInstanta = new TextBox() { Left = 450, Top = 50, Name = "textBoxInstanta" };
-            textBoxInstanta.Size = new System.Drawing.Size(100, 20);
+            TextBox textBoxInstanta = CreateTextBox("textBoxInstanta", "Instanta", 370, 20, 150, 20);
             panel.Controls.Add(textBoxInstanta);
+        }
+
+        private void RenderNumeInregistrat(Panel panel)
+        {
+            TextBox textBoxNumeInregistrat = CreateTextBox("textBoxNumeInregistrat", "Dosar inregistrat?", 20, 50, 150, 20);
+            panel.Controls.Add(textBoxNumeInregistrat);
+        }
+
+        private void RenderDataInregistrat(Panel panel)
+        {
+            TextBox textBoxDataInregistrat = CreateTextBox("textBoxDataInregistrat", "Data Inregistrat", 190, 50, 150, 20);
+            panel.Controls.Add(textBoxDataInregistrat);
         }
 
         private void RenderJudecatorie(Panel panel)
         {
-            Label textLabelJudecatorie = new Label() { Left = 350, Top = 80, Text = "Judecatorie" };
-            panel.Controls.Add(textLabelJudecatorie);
-            TextBox textBoxJudecatorie = new TextBox() { Left = 450, Top = 80, Name = "textBoxJudecatorie" };
-            textBoxJudecatorie.Size = new System.Drawing.Size(100, 20);
+            TextBox textBoxJudecatorie = CreateTextBox("textBoxJudecatorie", "Judecatorie", 370, 50, 150, 20);
             panel.Controls.Add(textBoxJudecatorie);
         }
 
         private void RenderFotoCopiat(Panel panel)
         {
-            Label textLabelFotoCopiat = new Label() { Left = 50, Top = 20, Text = "Foto Copiat" };
-            panel.Controls.Add(textLabelFotoCopiat);
-            TextBox textBoxFotoCopiat = new TextBox() { Left = 150, Top = 20, Name = "textBoxFotoCopiat" };
-            textBoxFotoCopiat.Size = new System.Drawing.Size(100, 20);
+            TextBox textBoxFotoCopiat = CreateTextBox("textBoxFotoCopiat", "Data Foto Copiat", 20, 80, 100, 20);
             panel.Controls.Add(textBoxFotoCopiat);
+        }
+
+        private void RenderMentiuni(Panel panel)
+        {
+            TextBox textBoxMentiuni = CreateTextBox("textBoxMentiuni", "Mentiuni", 150, 80, 370, 20);
+            panel.Controls.Add(textBoxMentiuni);
         }
 
         private void ComboBoxIndicativInstanta_SelectedIndexChanged(object sender, EventArgs e)
